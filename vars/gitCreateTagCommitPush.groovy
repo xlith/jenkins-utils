ab6c6e02e921559a16c76c9cf0847a5572eca3f1
Civan Yavuzşen <civanyavuzsen@gmail.com>
2022-03-17 15:00:45 +0100

    debug

--- vars/gitCreateTagCommitPush.groovy
+++ vars/gitCreateTagCommitPush.groovy
@@ -7,7 +7,7 @@ def call(String newVersion, String buildTicketID, String branchName) {
         git status
         git commit -am "${buildTicketID}: New version ready to be published ${newVersion}" &&
         git tag -a "${newVersion}" -m "${buildTicketID}: New version ${newVersion}" &&
-        git push --set-upstream origin ${branchName}:${branchName} &&
+        git push --set-upstream origin ${branchName} &&
         git push --tags &&
         git push origin &&
         git diff
