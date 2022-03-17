b20f50f8f1f4b615875e6111c24f77c1054cba44
Civan Yavuzşen <civanyavuzsen@gmail.com>
2022-03-17 14:01:32 +0100

    add copy artifacts method

--- vars/copyFolderToSharedDrive.groovy
+++ vars/copyFolderToSharedDrive.groovy
@@ -1,6 +1,24 @@
 #!/usr/bin/env groovy
 
 def call(String sourceDir, String remoteDir) {
-  echo "sourceDir: ${sourceDir}"
-  echo "remoteDir: ${remoteDir}"
+  cifsPublisher(publishers: [
+    [
+      configName: 'csnlfs01 - taggingproducts',
+      transfers: [
+        [
+          cleanRemote: false,
+          excludes: '',
+          flatten: false,
+          makeEmptyDirs: false,
+          noDefaultExcludes: false,
+          patternSeparator: '[, ]+',
+          remoteDirectory: "${sourceDir}",
+          remoteDirectorySDF: false,
+          removePrefix: '',
+          sourceFiles: "${remoteDir}"
+        ]],
+      usePromotionTimestamp: false,
+      useWorkspaceInPromotion: false,
+      verbose: false
+    ]])
 }
