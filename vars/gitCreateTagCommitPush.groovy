cf4cdfec716448cb1c285d2f7dba705b8c547e7e
Civan Yavuzşen <civanyavuzsen@gmail.com>
2022-03-18 12:09:14 +0100

    sorry 😔

--- vars/gitCreateTagCommitPush.groovy
+++ vars/gitCreateTagCommitPush.groovy
@@ -14,12 +14,3 @@ def call(String newVersion, String buildTicketID, String branchName) {
       """
   }
 }
-
-git diff &&
-        git status
-        git commit -am "TAG-1212: New version ready to be published 1.2.3" &&
-        git tag -a "1.2.3" -m "TAG-1212: New version 1.2.3" &&
-        git push --set-upstream origin main:main &&
-        git push --tags &&
-        git push origin &&
-        git diff
