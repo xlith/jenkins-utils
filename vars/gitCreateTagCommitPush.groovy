d25080e379634c9d9d2b23abd6b4a990dca1d22a
Civan Yavuzşen <civanyavuzsen@gmail.com>
2022-03-18 12:05:56 +0100

    debug

--- vars/gitCreateTagCommitPush.groovy
+++ vars/gitCreateTagCommitPush.groovy
@@ -7,10 +7,19 @@ def call(String newVersion, String buildTicketID, String branchName) {
         git status
         git commit -am "${buildTicketID}: New version ready to be published ${newVersion}" &&
         git tag -a "${newVersion}" -m "${buildTicketID}: New version ${newVersion}" &&
-        git push --set-upstream origin ${branchName}:${branchName} &&
+        git push --set-upstream origin ${branchName} &&
         git push --tags &&
         git push origin &&
         git diff
       """
   }
 }
+
+git diff &&
+        git status
+        git commit -am "TAG-1212: New version ready to be published 1.2.3" &&
+        git tag -a "1.2.3" -m "TAG-1212: New version 1.2.3" &&
+        git push --set-upstream origin main:main &&
+        git push --tags &&
+        git push origin &&
+        git diff
--- vars/mergeBranches.groovy
+++ vars/mergeBranches.groovy
@@ -6,7 +6,7 @@ def call(String sourceBranch, String targetBranch) {
         git fetch origin &&
         git checkout -b ${targetBranch} origin/${targetBranch} &&
         git merge ${sourceBranch} &&
-        git push -v --tags --set-upstream origin ${targetBranch}:${targetBranch} &&
+        git push -v --tags --set-upstream origin ${targetBranch} &&
         git diff
       """
   }
