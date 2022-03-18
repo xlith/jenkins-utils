b6996765260b210196bb47cf9da228d7f60992d6
Civan Yavuzşen <civanyavuzsen@gmail.com>
2022-03-18 15:12:17 +0100

    debug

--- vars/mergeBranches.groovy
+++ vars/mergeBranches.groovy
@@ -4,7 +4,7 @@ def call(String sourceBranch, String targetBranch) {
       sh """
         git fetch origin &&
         git checkout -b ${targetBranch} origin/${targetBranch} &&
-        git merge ${sourceBranch} &&
+        git merge origin/${sourceBranch} &&
         git push -v --tags --set-upstream origin HEAD:${targetBranch} &&
         git diff
       """
